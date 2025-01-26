8840e52f710f191420119cfde2a8e07f2c0e1deb
Majexs <166327239+Majexs@users.noreply.github.com>
2025-01-26 15:00:14 -0500

    added home and signup page visuals

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,25 @@
 import {Route, Routes} from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import SignUpPage from './pages/SignUpPage';
-import TransactionPage from './pages/TransactionPage';
-import NotFoundPage from './pages/NotFoundPage';
+import HomePage from './pages/HomePage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
+import TransactionPage from './pages/TransactionPage.jsx';
+import NotFoundPage from './pages/NotFoundPage.jsx';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
-
+  const authUser = true;
   return (
     <>
+      {authUser && <Header />}
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/signup' element={<SignUpPage />} />
-        <Route path='/transaction:id' element={<TransactionPage />} />
+        <Route path='/transaction/:id' element={<TransactionPage />} />
         <Route path='*' element={<NotFoundPage />} />
       </Routes>
+      <Footer />
     </>
   )
 }
--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -1,4 +1,64 @@
+import { useState } from "react";
+import {Link} from "react-router-dom";
+import RadioButton from "../components/RadioButton";
+import InputField from "../components/InputField";
+
 const SignUpPage = () => {
-    return <div>SignUpPage</div>
+    const [signUpData, setSignUpData] = useState({
+        name: "",
+        username: "",
+        password: "",
+        gender: "",
+    });
+
+    const handleChange = (e) => {
+        const{name, value, type} = e.target;
+
+        if (type === "radio") {
+            setSignUpData((prevData) => ({
+                ...prevData,
+                gender: value,
+            }));
+        } else {
+            setSignUpData((prevData) => ({
+                ...prevData,
+                [name]: value,
+            }));
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(signUpData);
+    }
+
+    return (
+    <div className='h-screen flex justify-center items-center'>
+        <div className='flex rounded-lg overflow-hidden z-50 bg-gray-300'>
+            <div className='w-full bg-gray-100 min-w-80 sm:min-w-96 flex items-center justify-center'>
+                <div className='max-w-md w-full p-6'>
+                    <h1 className='text-3xl font-semibold mb-6 text-black text-center'>Sign Up</h1>
+                    <h1 className='text-sm font-semibold mb-6 text-gray-500 text-center'>
+                        Join to keep track of your expenses
+                    </h1>
+                    <form className='space-y-4' onSubmit={handleSubmit}>
+                        <InputField 
+                        
+                        />
+                        <InputField 
+                        
+                        />
+                        <InputField 
+                        
+                        />
+                        <InputField 
+                        
+                        />
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
 };
 export default SignUpPage;
